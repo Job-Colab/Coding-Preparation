1612411a92be5ff96ad97f36a4aa39cb116bf91c
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-10-05 21:59:53 +0530

    Corect me if any mistakes,also i can learn

--- Day-01/Mohan.js
+++ Day-01/Mohan.js
@@ -1,14 +1,14 @@
 function multiplyMatrices(matrix1, matrix2) {
   var result = [];
   for (var i = 0; i < matrix1.length; i++) {
-    console.log("I", i);
+    // console.log("I", i);  //!Just for myself to know the values of i j and k
     result[i] = [];
     for (var j = 0; j < matrix2[0].length; j++) {
-      console.log("J", j);
+      //   console.log("J", j); //!Just for myself to know the values of i j and k
 
       var sum = 0;
       for (var k = 0; k < matrix1[0].length; k++) {
-        console.log("k", k, i);
+        // console.log("k", k, i); //!Just for myself to know the values of i j and k
 
         sum += matrix1[i][k] * matrix2[k][j];
       }
