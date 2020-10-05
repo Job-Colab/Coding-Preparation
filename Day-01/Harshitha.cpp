9907e16ed2b7c48db6dd267b1ab62a3aaf0fc8c7
Harshitha <harshiling@gmail.com>
2020-10-05 21:26:23 +0530

    Solved Day-01

---
+++ Day-01/Harshitha.cpp
@@ -0,0 +1,18 @@
+
+void multiply(int A[][100], int B[][100], int C[][100], int N)
+{
+    
+ 
+     for(int i = 0 ; i < N ; i++)
+     {
+         for(int j = 0 ; j < N ; j++)
+         {
+             C[i][j] = 0;
+             for(int k = 0 ; k < N ; k++)
+             {
+                 C[i][j] += A[i][k] * B[k][j];
+             }
+         }
+     }
+     
+}
