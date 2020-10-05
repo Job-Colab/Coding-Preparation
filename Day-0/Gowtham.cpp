1720f2532e424d543d23fa452ed03612f0e44582
Gowtham B <63305824+GowthamGoush@users.noreply.github.com>
2020-10-05 11:31:15 +0530

    Initial commit

---
+++ Day-0/Gowtham.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+   	char Greeting = 72;
+   	
+  	cout<<Greeting;
+  	
+  	Greeting -= 3; 
+  	cout<<Greeting;
+  	
+  	Greeting += 7; 
+  	cout<<Greeting<<Greeting;
+  	
+  	Greeting += 3; 
+  	cout<<Greeting;
+  	
+  	Greeting -= 46; 
+  	cout<<Greeting;
+  
+	return 0;
+}
+
