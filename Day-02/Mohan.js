119f4ee2b7394c0cfe552e825ffd42b37f1f178d
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-10-06 09:23:26 +0530

    Updated Day-02

--- Day-02/Mohan.js
+++ Day-02/Mohan.js
@@ -1,3 +1,5 @@
+//! Using functional programming
+
 "use-strict";
 
 const inputElements = [3, 2, 4, 6, 7, 5, 8, 0, 1];
@@ -15,3 +17,19 @@ const sumOfOdd = (odd) =>
 
 const result = sumOfOdd(oddElements);
 console.log(result);
+
+// ! Normal approach
+const inputElements = [3, 2, 4, 6, 7, 5, 8, 0, 1];
+
+let i = 0;
+let sumOfOdd = 0;
+
+for (i = 0; i < inputElements.length; i++) {
+  if (inputElements[i] & 1) {
+    sumOfOdd += inputElements[i];
+  } else {
+    continue;
+  }
+}
+
+console.log(sumOfOdd);
