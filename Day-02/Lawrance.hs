ea060ede7df82172f0cdc37bfc283d565caff78d
law27 <40236169+law27@users.noreply.github.com>
2020-10-06 13:18:16 +0530

    Update Lawrance.hs

--- Day-02/Lawrance.hs
+++ Day-02/Lawrance.hs
@@ -1,4 +1,4 @@
-f arr = sum [ x | x <- arr, x `mod` 2 == 1]
+f arr = sum [ x | x <- arr, odd x]
 
 -- This part handles the Input/Output and can be used as it is. Do not change or modify it.
 main = do
