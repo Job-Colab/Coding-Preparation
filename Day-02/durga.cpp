280a1fc27837c81892e5128962b53f38f60e9281
durgasneha <54586751+durgasneha@users.noreply.github.com>
2020-10-06 20:38:11 +0530

    Create durga.cpp

---
+++ Day-02/durga.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum+=arr[i];
+        }
+    }
+    cout<<sum;
+}
