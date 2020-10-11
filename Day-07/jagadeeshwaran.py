0f6fdc6ed451be1c3df1477349fbd0173e4aad3b
jagadeeshwaran001 <45691973+jagadeeshwaran001@users.noreply.github.com>
2020-10-11 11:20:07 +0530

    Create jagadeeshwaran.py

---
+++ Day-07/jagadeeshwaran.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        a=''
+        self.strs = strs
+        for i in range(len(strs)):
+            for j in range(1,len(strs[0])+1):
+                try:
+                    if(strs[i][:j] == strs[i+1][:j]):
+                        a = strs[i][:j]
+                    else:
+                        break
+                except IndexError:
+                    pass
+            
+        return(a)            
+if __name__ == '__main__':
+    obb=Solution()
+    strs = ["flower","flow","flight"]#list(map(int,input().split()))
+    print(""" "%s" """%obb.longestCommonPrefix(strs))
