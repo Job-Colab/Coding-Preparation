acf111b753c03409c61da171d998447c81e2fc54
Lavanyamanohar11 <55652714+Lavanyamanohar11@users.noreply.github.com>
2020-10-11 18:34:26 +0530

    day-07

---
+++ Day-07/lavanya.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        if(strs.size()==0)
+            return ans;
+        for(int i=0; i< strs[0].size();i++){
+            int j=1;
+            for(; j<strs.size() && strs[j].size() > i; j++){
+                if(strs[0][i] != strs[j][i])
+                    return ans;
+            }
+             if(j == strs.size()){
+                    ans+= strs[0][i];
+                }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> str = {"flower","flow","flight"};
+    cout<<s.longestCommonPrefix(str);
+    
+}
