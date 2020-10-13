d8ad7e7b76a13b30fa2f1a597e7e5d6090c9c423
Harshiling <harshiling@gmail.com>
2020-10-13 11:44:20 +0530

    Done

---
+++ Day-09/Harshitha.cpp
@@ -0,0 +1,65 @@
+//Find the fine : gfg
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+    
+    while(testCase--)
+    {
+    int N , D;
+    cin>>N;
+    cin>>D;
+    
+    int cars[N] , penalty[N] ;
+    
+    for(int i = 0 ; i < N ; i++)
+    {
+        cin>>cars[i];
+    }
+    
+    for(int i = 0 ; i < N ; i++)
+    {
+        cin>>penalty[i];
+    }
+    
+    int fine = 0;
+    
+    for(int i = 0 ; i < N ; i++)
+    {
+       if(D % 2 == 0) //even
+       {
+       if(i % 2 == 0)
+       {
+           fine += penalty[i];
+       }
+       }
+       if( D % 2 != 0)
+       {   
+           if( i % 2 != 0)
+           fine += penalty[i];  
+       }
+    
+    
+    }
+    cout<<fine<<'\n';
+}
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
