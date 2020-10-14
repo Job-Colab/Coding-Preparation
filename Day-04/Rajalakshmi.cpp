e0d164449563f5111617bfef95c23afc9cd9c783
Rajalakshmi <54462305+kajalmishra7@users.noreply.github.com>
2020-10-14 08:51:53 +0530

    Day4

---
+++ Day-04/Rajalakshmi.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j;
+        vector<int> ans;
+        for(i=0;i<nums.size();i++)
+        {
+            for(j=i+1;j<nums.size();j++)
+            if(nums[i]+nums[j]==target )
+            {
+                ans.push_back(i);
+                ans.push_back(j);          
+            }
+        }
+        return ans;
+    }
+	int main() {
+    int n, x, tar, i;
+    vector<int> a;
+    cin>>n;
+    for(i=0; i<n; i++){
+        cin>>x;
+        a.push_back(x);
+    }
+        
+    cin>>tar;
+    
+    vector<int> ans = twoSum(a, tar);
+    
+    cout<<ans[0]<<", "<<ans[1]<<"\n";
+    
+    return 0;
+}
