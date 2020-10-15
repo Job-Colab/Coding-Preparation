9810149d8289f304f3250e2630bea27ce4b13a0d
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-10-15 13:46:11 +0530

    Somehow ....

---
+++ Day-10/Mohan.js
@@ -0,0 +1,34 @@
+var letterCombinations = (digits) => {
+  if (digits === "") {
+    return [];
+  }
+  const numberAndLetters = {
+    1: [],
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "y", "x", "z"],
+  };
+
+  let combinations = [];
+  combinations = [...numberAndLetters[digits[0]]];
+
+  for (let i = 1; i < digits.length; i++) {
+    let updatedCombo = [];
+    combinations.forEach((letter) => {
+      numberAndLetters[digits[i]].forEach((companionLetter) => {
+        updatedCombo.push(letter + companionLetter);
+      });
+    });
+    combinations = updatedCombo;
+  }
+
+  return combinations;
+};
+
+const result = letterCombinations("23");
+console.log(result);
