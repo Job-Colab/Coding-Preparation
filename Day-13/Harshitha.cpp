725ee67c0e2efc2b2dd11af3d3ff4a679444c312
Harshitha <harshiling@gmail.com>
2020-10-17 23:06:32 +0530

    Done

---
+++ Day-13/Harshitha.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        int count = 0 , n = nums.size();
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        
+        
+        return count;
+    }
+};
