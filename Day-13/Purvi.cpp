525a23fb9284518dbeb7b1286d96964deba03123
Purvi <51371107+purvi-n@users.noreply.github.com>
2020-10-17 19:08:42 +0530

    Delete Purvi.cpp

--- Day-13/Purvi.cpp
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        c=nums.count(val)
-        for i in range(c):
-            nums.remove(val)
-        return len(nums)
