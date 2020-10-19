f6bf828f0ac94a0baeadbc8400a3685f8aed0c07
Lawrance <jeyalawrance@gmail.com>
2020-10-19 09:25:02 +0530

    🎊🎉

---
+++ Day-15/Lawrance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        for(int i = 0; i < prices.size() - 1; i++) {
+            for(int j = i + 1; j < prices.size(); j++) {
+                if(prices[i] >= prices[j]) {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};
