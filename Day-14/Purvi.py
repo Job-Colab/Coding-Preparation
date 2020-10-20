06f7be8895094b31178edac1b5e7b55abdeda634
Purvi <51371107+purvi-n@users.noreply.github.com>
2020-10-20 18:21:58 +0530

    day 14

---
+++ Day-14/Purvi.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(0, len(matrix)-1):
+            for j in range(0, len(matrix[0])-1):
+                if(matrix[i][j]!=matrix[i+1][j+1]):
+                    return False
+        return True
