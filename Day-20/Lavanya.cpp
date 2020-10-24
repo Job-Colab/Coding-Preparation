9d76681c0cf2a6d8b9a210172b0119780a2845af
Lavanyamanohar11 <55652714+Lavanyamanohar11@users.noreply.github.com>
2020-10-24 14:20:50 +0530

    done

---
+++ Day-20/Lavanya.cpp
@@ -0,0 +1,17 @@
+/**
+ * class LLNode {
+ *     public:
+ *         int val;
+ *         LLNode *next;
+ * };
+ */
+bool solve(LLNode* head) {
+    LLNode *temp = head;
+    while(temp->next != NULL){
+        if(temp->val >= temp->next->val){
+            return false;
+        }
+        temp = temp->next;
+    }
+    return true;
+}
