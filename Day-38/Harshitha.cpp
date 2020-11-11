52b9d0c9b6c3fd35bb8d6749e554957b0ba4df64
Harshiling <harshiling@gmail.com>
2020-11-11 12:29:01 +0530

    Good Problem , learnt a new way :)

---
+++ Day-38/Harshitha.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public :
+    int maxProfit(vector<int> &prices) {
+        int maxprofit = 0;
+        int buy = INT_MAX;
+        for(int i = 0 ; i < prices.size() ;i++)
+        {
+            buy = min(buy , prices[i]);
+            maxprofit= max(maxprofit , prices[i] - buy);
+        }
+        return maxprofit;
+    }
+};
