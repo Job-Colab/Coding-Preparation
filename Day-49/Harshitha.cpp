b77703917c94470c34e2ff0ed104221ce61bae4f
Harshitha <harshiling@gmail.com>
2020-11-22 15:26:41 +0530

    Done done :party:

---
+++ Day-49/Harshitha.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        
+        map<int , int>lucky;
+        vector<int>nos;
+        for(int i = 0 ; i < arr.size() ; i++)
+        {
+            lucky[arr[i]]++;
+        }
+        int nothing = 0;
+        
+        for(auto i : lucky)
+        {
+             if(i.first == i.second)
+            {
+                nos.push_back(i.first);
+            }
+        }
+        
+     if(nos.size() >= 1)
+     {
+     return *max_element(nos.begin() , nos.end());
+     }
+     else
+     {
+     return -1;
+     }
+     
+    }
+};
