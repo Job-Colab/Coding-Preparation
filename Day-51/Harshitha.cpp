133bbb3ab656bbde6b5eb0e648384426ef638cd3
Harshitha <harshiling@gmail.com>
2020-11-24 11:38:29 +0530

    Referred Law

---
+++ Day-51/Harshitha.cpp
@@ -0,0 +1,26 @@
+/* The method push to push element into the queue */
+void StackQueue :: push(int x)
+ {
+      
+      s1.push(x);
+ }
+
+/*The method pop which return the element poped out of the queue*/
+int StackQueue :: pop()
+{
+    if(s1.size() == 0)return -1;
+    
+    while(s1.size() != 1)
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    int temp = s1.top();
+    s1.pop();
+    while(!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+    return temp;
+}
