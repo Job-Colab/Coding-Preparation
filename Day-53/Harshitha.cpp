ba7930773d80939dc5bb9439fbc2d0fbe9f2ce4d
Harshitha <harshiling@gmail.com>
2020-11-26 11:20:31 +0530

    Dandanakka

---
+++ Day-53/Harshitha.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) 
+    {
+        vector<int>changed;
+        changed = heights;
+        sort(heights.begin() , heights.end());
+        
+        int move = 0;
+        
+        for(int i = 0 ; i < heights.size(); ++i)
+        {
+            if(abs(heights[i] - changed[i]) != 0)
+                move++;
+        }
+        return move;
+        
+    }
+};
