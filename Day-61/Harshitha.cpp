fa3fea191972030ff03a2589855ac956b11b23d0
Harshitha <harshiling@gmail.com>
2020-12-04 12:44:07 +0530

    Done

---
+++ Day-61/Harshitha.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0 , ten = 0 ;
+        
+        for(auto i : bills){
+            if(i == 5)
+                five++;
+            else if( i == 10 ){
+                if(five == 0)return false;
+                --five;
+                ++ten;
+            }
+            else { 
+            if (five > 0 && ten > 0){
+                --five;
+                --ten; }
+            
+            else if(five >= 3){
+                five -= 3;
+            }
+            else{
+                return false;
+            }
+    }
+    }
+         return true;
+    }
+};
