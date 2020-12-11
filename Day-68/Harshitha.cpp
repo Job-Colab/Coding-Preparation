fac94e0e750f08907c3fb590aa2ca1982708b30c
Harshitha <harshiling@gmail.com>
2020-12-11 12:15:44 +0530

    DOne woo hoo

---
+++ Day-68/Harshitha.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        
+        int count=0;
+        
+        for(int i =0 ; i< nums.length; i++){
+            
+            if((nums[i]>9 && nums[i]<100) || (nums[i]>999 && nums[i]<10000) || nums[i] == 100000){
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
