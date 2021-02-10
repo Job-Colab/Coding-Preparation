9eb39b45a78d6824de9b365eae1b86cec5a51e61
Harshitha <harshiling@gmail.com>
2021-02-10 22:29:07 +0530

    Update Harshitha.cpp

--- Day-84/Harshitha.cpp
+++ Day-84/Harshitha.cpp
@@ -17,3 +17,49 @@ class Solution {
        }
        
 };
+/*//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+class Solution {
+  public:
+       int RedOrGreen(int N,string S) {
+       int countRed = 0, countGreen = 0 ;
+       for(int i = 0 ; i < S.length() ; ++i)
+       {
+           if(S[i] == 'R')
+           {
+               ++countRed;
+           }
+           else
+           {
+               ++countGreen;
+           }
+       }
+      return countRed > countGreen ? countGreen : countRed ;
+       
+       }
+       
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.RedOrGreen(N,S) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
+*/
