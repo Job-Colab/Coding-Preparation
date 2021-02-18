f137a11c8e6b76c738e3dfa04db3786c9bcbcd20
jagadeeshwaran001 <45691973+jagadeeshwaran001@users.noreply.github.com>
2021-02-18 23:04:29 +0530

    Create jagadeeshwaran.cpp

---
+++ Day-92/jagadeeshwaran.cpp
@@ -0,0 +1,17 @@
+void SortedStack :: sort()
+{
+   //Your code here
+   int size = s.size();
+   int* arr = new int[size];
+   int i = 0;
+   while(!s.empty())
+   {
+       arr[i++] = s.top();
+       s.pop();
+   }
+   ::sort(arr,arr+size);
+   for(int i = 0 ; i < size ; i++)
+   {
+       s.push(arr[i]);
+   }
+}
