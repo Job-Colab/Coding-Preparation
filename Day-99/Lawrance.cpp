f01f863539358426ec4b532d4df7481d308f5b1c
Lawrance <jeyalawrance@gmail.com>
2021-02-24 07:14:31 +0530

    Create Lawrance.cpp

---
+++ Day-99/Lawrance.cpp
@@ -0,0 +1,12 @@
+string solve(string s) {
+    string ans = "";
+    for(int i = 0; i < s.size(); i++) {
+        char temp = s[i];
+        while(temp == s[i]) {
+            ++i;
+        }
+        --i;
+        ans += temp;
+    }
+    return ans;
+}
