85b6260fd2b8fd990a2c7f8d9c322d5a0087ca32
Lawrance <jeyalawrance@gmail.com>
2021-04-23 18:53:11 +0530

    Create Lawrance.cpp

---
+++ Day-127/Lawrance.cpp
@@ -0,0 +1,18 @@
+long long int count(long long int n)
+{
+	long long int table[n+1],i;
+	memset(table, 0, sizeof(table));
+	table[0]=1;                 // If 0 sum is required number of ways is 1.
+	int num = 3;
+	for(int i = 3; i <= n; i++) {
+	    table[i] += table[i - 3];
+	}
+	for(int i = 5; i <= n; i++) {
+	    table[i] += table[i - 5];
+	}
+	for(int i = 10; i <= n; i++) {
+	    table[i] += table[i - 10];
+	}
+	
+	return table[n];
+}
